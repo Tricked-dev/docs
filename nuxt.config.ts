c49a1df84bea5d542324d8bd0f7b044dd2138147
Tricked <tricked@tricked.pro>
2023-07-20 23:46:55 +0200

    fix build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,13 @@
+import path from "path";
+
 export default defineNuxtConfig({
   // https://github.com/nuxt-themes/docus
   extends: "@nuxt-themes/docus",
-
   modules: [
     // https://github.com/nuxt/devtools
     "@nuxt/devtools",
   ],
+  alias: {
+    nanoid: path.resolve(__dirname, "node_modules/nanoid/index.browser.js"),
+  },
 });
